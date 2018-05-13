01938a923f524d0a6326ae93bd8447348477ecd2
yang <642444906@qq.com>
2018-05-13 22:29:04 +0800

    Clean useless source code

--- src/main/java/com/my/project/hotrefresh/HotRefreshApplication.java
+++ src/main/java/com/my/project/hotrefresh/HotRefreshApplication.java
@@ -1,14 +1,8 @@
 package com.my.project.hotrefresh;
 
-import java.net.MalformedURLException;
-
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.UrlResource;
 
 @Configuration
 @SpringBootApplication
@@ -18,12 +12,4 @@ public class HotRefreshApplication {
 		SpringApplication.run(HotRefreshApplication.class, args);
 	}
 
-	@Bean
-	public YamlPropertiesFactoryBean yamlProperties() throws MalformedURLException {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		YamlPropertiesFactoryBean yamlProperties = new YamlPropertiesFactoryBean();
-		yamlProperties.setResources(new UrlResource("http://localhost:8080/application-extends.yml"));
-		propertySourcesPlaceholderConfigurer.setProperties(yamlProperties.getObject());
-		return new YamlPropertiesFactoryBean();
-	}
 }
